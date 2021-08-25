c545eaf91413c6a7ef5aca982bb9b5f51e86894d
takuya04 <takuya.ct0418@gmail.com>
2021-08-25 11:20:51 +0900

    1st commit for clone

---
+++ src/main/java/com/airs/demo/StsGitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.airs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGitCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGitCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airs/demo/StsGitCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsGitCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
